cc19c02e2cd0e62063f262d94b243ad8f54775ab
Jahangir <md.jahangir27@gmail.com>
2016-10-31 00:30:08 +0530

    Add get functionality with path parameter.

--- src/main/scala/todos/Handler.scala
+++ src/main/scala/todos/Handler.scala
@@ -1,14 +1,12 @@
 package todos
 
 import java.util.UUID
-
 import awscala._
 import awscala.dynamodbv2._
 import com.amazonaws.services.lambda.runtime.Context
 import org.json4s._
 import org.json4s.native.Serialization
 import org.json4s.native.Serialization._
-
 import scala.beans.BeanProperty
 /**
   * Created by jahangir on 10/30/16.
@@ -33,12 +31,27 @@ class Handler {
     Response(200,s"{id: $id}")
   }
 
+  def getOne(input: GetOneRequest, context: Context): Response = {
+    val res = table.get(input.getPathParameters.id)
+    val item = res.get.attributes.find(_.name == "item").get.value.s.get
+    Response(200,s"$item")
+  }
+
+
 }
 
 class GetAllRequest
 
 class CreateRequest(@BeanProperty var body: String) {
   def this() = this("")
+}
+
+case class Data(@BeanProperty var id: String) {
+  def this() = this("")
+}
+
+class GetOneRequest(@BeanProperty var pathParameters: Data) {
+  def this() = this(Data(""))
 }
 
 case class Response(@BeanProperty statusCode: Int, @BeanProperty body: String)
